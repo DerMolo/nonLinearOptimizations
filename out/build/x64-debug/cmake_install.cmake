23046a814a5729375657e0f0e2278ad66a9d1c0a
DerMolo <Iampixel134@gmail.com>
2024-11-21 03:41:44 -0500

    new changes

--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Iampi/source/repos/CmakeTest
+# Install script for directory: C:/Users/CARME/source/repos/nonLinearOptimizations
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Iampi/source/repos/CmakeTest/out/install/x64-debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/CARME/source/repos/nonLinearOptimizations/out/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Iampi/source/repos/CmakeTest/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/CARME/source/repos/nonLinearOptimizations/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
